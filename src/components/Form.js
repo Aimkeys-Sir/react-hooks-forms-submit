474cb0de9ad6abd4a162de91cb2d16d45d2b0f49
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-15 11:16:21 +0300

    commit tree 02fd60f1f5a97ec2accbce6b04ec9142af76b7e3

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([])
+  const [err, setErrors] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -10,14 +12,43 @@ function Form(props) {
 
   function handleLastNameChange(event) {
     setLastName(event.target.value);
+
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+    const formData = {
+      firstName: firstName,
+      lastName: lastName
+    }
+    if (firstName.length > 1) {
+      const allData = [...submittedData, formData]
+      setSubmittedData(allData)
+      setFirstName("")
+      setLastName("")
+      setErrors([])
+    } else {
+      setErrors(["A name should be at least 2 characters long!"])
+    }
   }
 
+  const listOfNames = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.lastName}, {data.firstName}
+      </div>
+    )
+  })
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      {err.length>0?err.map((error,index)=><p key={index}>{error}</p>):null}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="first name" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" placeholder="last name" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {listOfNames}
+    </div>
+
   );
 }
 
